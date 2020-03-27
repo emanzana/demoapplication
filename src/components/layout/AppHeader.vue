c7dc5b258f7170136a0a3dedfc9271e96f4f8613
Enrique Manzanedo Alted <SURRV0470K@externo.alisedainmobiliaria.com>
2020-03-27 14:52:45 +0100

    cambios Oscar barra navegación

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,14 +1,14 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark" >
-    <b-navbar-brand href="/">Acciona</b-navbar-brand>
+    <b-navbar-brand href="/"><h2>Acciona</h2></b-navbar-brand>
  
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
  
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="/projects">Projects</b-nav-item>
-        <b-nav-item href="/countries">Countries</b-nav-item>
-        <b-nav-item href="/capextypes">Capextypes</b-nav-item>
+        <b-nav-item href="/projects"><h4>Projects</h4></b-nav-item>
+        <b-nav-item href="/countries"><h4>Countries</h4></b-nav-item>
+        <b-nav-item href="/capextypes"><h4>Capextypes</h4></b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
